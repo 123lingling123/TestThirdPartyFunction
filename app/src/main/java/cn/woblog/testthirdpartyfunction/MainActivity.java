4b2c02d6cada04dd132690951c6e13e9d6d5e3e2
30234@163.com <lifeng>
2016-04-08 17:07:50 +0800

    add share image to QQ or QZone

--- app/src/main/java/cn/woblog/testthirdpartyfunction/MainActivity.java
+++ app/src/main/java/cn/woblog/testthirdpartyfunction/MainActivity.java
@@ -1,7 +1,11 @@
 package cn.woblog.testthirdpartyfunction;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import cn.woblog.testthirdpartyfunction.activity.QQActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void testQQ(View view) {
+        startActivity(new Intent(MainActivity.this, QQActivity.class));
     }
 }
